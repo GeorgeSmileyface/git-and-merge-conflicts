392527131bfc77b15288672ce4f4dc0996c04edf
V-Ragn <lborgm@gmail.com>
2025-05-05 10:32:24 +0200

    Davs

--- git and merge conflicts/Program.cs
+++ git and merge conflicts/Program.cs
@@ -5,6 +5,7 @@ Console.WriteLine("Hello, World!");
 //added something else
 //added something
 //some other line
+//Davs
 //This is a new line
 //This is another new line
 //This is an existing line
